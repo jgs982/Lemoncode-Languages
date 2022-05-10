20264da66e54976a9ea8c9e6778d2d4e6ab8a122
Jesús Guerrero <jgs982@gmail.com>
2022-05-10 11:57:30 +0200

    Solucion Deliverable 01

--- src/01 deliverables/exercise-01.ts
+++ src/01 deliverables/exercise-01.ts
@@ -1,29 +1,50 @@
 console.log("************** DELIVERABLE 01 *********************");
 
 /*
-    ### 1. Array operations
+    1. Array operations
 
-    #### Head
-    
+    # Head    
     Implementa una función `head` (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento. Utiliza destructuring.
-    
-    const head = (// array //) => {}; // Implementation here.
-        
-    #### Tail
-    
+           
+    # Tail    
     Implementa una función `tail` (inmutable), tal que, dado un array como entrada devuelta todos menos el primer elemento. Utiliza rest operator.
-    
-    const tail = (// array //) => {}; // Implementation here.
         
-    #### Init
-    
+    # Init    
     Implementa una función `init` (inmutable), tal que, dado un array como entrada devuelva todos los elementos menos el último. Utiliza los métodos que ofrece Array.prototype.
-    
-    const init = (// array //) => {}; // Implementation here.
-        
-    #### Last
-    
-    Implementa una función `last` (inmutable), tal que, dado un array como entrada devuelva el último elemento.
-    
-    const last = (// array //) => {}; // Implementation here.    
+           
+    # Last    
+    Implementa una función `last` (inmutable), tal que, dado un array como entrada devuelva el último elemento.    
 */
+
+const array = [1, 2, 3, 4, 5];
+console.log(`Array: ${array}`);
+
+const head = (array) => {    
+    let [primero, _] = array;
+    return primero;
+};
+
+const first = head(array);
+console.log(`El primer elemento del array dado es: ${first}`);
+
+const tail = (array) => {
+    let [head, ...tail] = array;
+    return tail;
+};
+
+const cola = tail(array);
+console.log(`La cola del array dado es: ${cola}`);
+
+const init = (array) => {
+    return array.slice(0, array.length - 1);
+};
+
+const cabeza = init(array);
+console.log(`Elementos del array menos el último: ${cabeza}`);
+
+const last = (array) => {
+   return array.slice(-1);
+};
+
+const ultimo = last(array);
+console.log(`El último elemento del array es: ${ultimo}`);
