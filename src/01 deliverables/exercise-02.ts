0db6b03fa5d53b0b1bcb4a484e5161f767a02ccb
Jesús Guerrero <jgs982@gmail.com>
2022-05-10 12:15:54 +0200

    Solucion Deriverable 02

--- src/01 deliverables/exercise-02.ts
+++ src/01 deliverables/exercise-02.ts
@@ -1,13 +1,27 @@
 console.log("************** DELIVERABLE 02 *********************");
 
 /*
-    ### 2. Concat
+    2. Concat
     
     Implementa una función `concat` (inmutable) tal que, dados 2 arrays como entrada, devuelva la concatenación de ambos. Utiliza rest / spread operators.
-    
-    const concat = (a, b) => {}; // Implementation here.
-        
-    #### Opcional
-    
+            
+    Opcional:    
     Implementa una versión del ejercicio anterior donde se acepten múltiples arrays de entrada (más de 2).
 */
+
+const arrayA = ['a1', 'a2', 'a3', 'a4'];
+const arrayB = ['b1', 'b2', 'b3'];
+const arrayC = ['c1', 'c2', 'c3', 'c4', 'c5'];
+const arrayD = ['d1'];
+
+const concat = (a, b, ...resto) => {
+    return [...a, ...b, ...resto];
+};
+
+const concatenado = concat(arrayA, arrayB, arrayC, arrayD);
+
+console.log(`Array A: ${arrayA}`);
+console.log(`Array B: ${arrayB}`);
+console.log(`Array C: ${arrayC}`);
+console.log(`Array D: ${arrayD}`);
+console.log(`Concatenación: ${concatenado}`);
