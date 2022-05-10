7d1b47f4ccd26d673196e5861c4ec15bf7cbb58f
Jesús Guerrero <jgs982@gmail.com>
2022-05-10 12:59:01 +0200

    Solucion Deliverable 04

--- src/01 deliverables/exercise-04.ts
+++ src/01 deliverables/exercise-04.ts
@@ -1,29 +1,32 @@
 console.log("************** DELIVERABLE 04 *********************");
 
 /*
-    ### 4. Read Books
+    4. Read Books
     
     Crea una función isBookRead que reciba una lista de libros y un título y devuelva si se ha leído o no dicho libro.
     Un libro es un objeto con `title` como string y `isRead` como booleano. En caso de no existir el libro devolver `false`
     TIP: Existe un método de Array.prototype que te ayudará a buscar según un patrón.
-    
-    function isBookRead(books, titleToSearch) {
-      // Implementation here
-    }
-        
-    #### Ejemplo
-    
-    const books = [
-      { title: "Harry Potter y la piedra filosofal", isRead: true },
-      { title: "Canción de hielo y fuego", isRead: false },
-      { title: "Devastación", isRead: true },
-    ];
-    
-    console.log(isBookRead(books, "Devastación")); // true
-    console.log(isBookRead(books, "Canción de hielo y fuego")); // false
-    console.log(isBookRead(books, "Los Pilares de la Tierra")); // false
-        
-    #### Opcional
-    
-    Utiliza Typescript para añadir los tipos adecuados.
 */
+
+function isBookRead(books, titleToSearch) 
+{ 
+  for(const key in books) 
+  {
+    if(books[key]['title']===titleToSearch)
+    {
+      return books[key]['isRead'];
+    }      
+  }
+
+  return false;
+}
+
+const books = [
+  { title: "Harry Potter y la piedra filosofal", isRead: true },
+  { title: "Canción de hielo y fuego", isRead: false },
+  { title: "Devastación", isRead: true },
+];
+
+console.log(isBookRead(books, "Devastación")); 
+console.log(isBookRead(books, "Canción de hielo y fuego")); 
+console.log(isBookRead(books, "Los Pilares de la Tierra")); 
